a95f42ddf05495b949381e72a67718293c475b2a
NanFeng Cheong <Cheong_Nan@jabil.com>
2017-08-11 22:03:48 +0800

    update

--- FoodsterSln/Data/Migrations/20170811122426_first init.Designer.cs
+++ FoodsterSln/Data/Migrations/20170811122426_first init.Designer.cs
@@ -8,9 +8,10 @@ using FoodsterSln.Data;
 namespace FoodsterSln.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170811122426_first init")]
+    partial class firstinit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
