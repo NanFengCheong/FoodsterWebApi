84b0b80d03f488051ceef41f7036895cd6ac3581
Johnson Pang <fireprince1024@hotmail.com>
2017-08-12 10:30:57 +0800

    add in usertagmodels

---
+++ FoodsterApp/Models/UserTagModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FoodsterApp.Models
+{
+    public class UserTagModels
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        [ForeignKey("Tag")]
+        public int TagID { get; set; }
+        public TagModel Tag { get; set; }
+    }
+}
